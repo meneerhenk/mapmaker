d868a85292747e73bc14a30c7f1621625aca48fb
Mendel Mobach <mendel@mobach.nl>
2020-10-02 14:32:44 +0200

    Oude data uitfilteren, datum parsing compatible met RIVM changes

--- mapmaker.py
+++ mapmaker.py
@@ -3,6 +3,7 @@ import urllib2
 import json
 import pickle
 import math
+from datetime import datetime, timedelta
 postcodes = {}
 vals = {}
 dates = {}
@@ -14,12 +15,20 @@ except IOError:
     pass
 f = urllib2.urlopen('https://data.rivm.nl/covid-19/COVID-19_rioolwaterdata.json')
 data = json.loads(f.read())
+
+last = datetime.strptime('1970-01-01',"%Y-%m-%d");
+for block in data:
+  datum = datetime.strptime(block['Date_measurement'],"%Y-%m-%d" if block['Date_measurement'].startswith("2020") else "%d-%m-%Y");
+  if datum > last:
+      last = datum
+
+twoweek = last - timedelta(days=14)
 for block in data:
   pc = block['Postal_code']
-  if pc != '':
+  if pc != '' and datetime.strptime(block['Date_measurement'],"%Y-%m-%d" if block['Date_measurement'].startswith("2020") else "%d-%m-%Y") >= twoweek:
       if pc not in postcodes:
-            print(pc)
-            print(block)
+            #print(pc)
+            #print(block)
             f = urllib2.urlopen('https://api.opencagedata.com/geocode/v1/json?q='+pc+'%20'+'%20'+(block['RWZI_AWZI_name'].replace(' ','%20'))+'%20Netherlands&key=5ef2349fa1a8489fad3a33c34ff4b15a&no_annotations=1&language=nl')
             data = f.read()
             js = json.loads(data)
@@ -34,6 +43,8 @@ for block in data:
                 postcodes[pc] = js['results'][0]['geometry']
       vals[pc] = block['RNA_per_ml']
       dates[pc] = block['Date_measurement']
+
+#print "Aantal locaties",len(vals)
 
 percolor = float(256*2) / math.log(1041 if max(vals.values()) < 1041 else max(vals.values()))
 for pc in vals:
